264108198b48cbfa4399c8110e64aa0bf5de6689
Mis25786 <111245815+Mis25786@users.noreply.github.com>
2023-06-12 18:23:06 +0200

    add .env.example end port to server.js - lessons-3_1 complete

--- server.js
+++ server.js
@@ -2,21 +2,16 @@ const mongoose = require("mongoose");
 
 const app = require("./app");
 
-// const DB_HOST = process.env.DB_HOST;
-const { DB_HOST } = process.env;
+const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose.set("strictQuery", true);
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3000);
+    app.listen(PORT);
   })
   .catch((err) => {
     console.log(err.message);
     process.exit(1);
   });
-
-// app.listen(3000, () => {
-//   console.log("Server running. Use our API on port: 3000");
-// });
