c53fc729ca1da1ee29e2469c8018ef23fb5607c4
Mis25786 <111245815+Mis25786@users.noreply.github.com>
2023-06-12 15:43:13 +0200

    DB_HOST add

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+// const dotenv = require("dotenv");
+const { DB_HOST } = require("./config");
 
-dotenv.config({ path: "./.env" });
+// dotenv.config({ path: "./.env" });
 
 const app = require("./app");
 
-const DB_HOST = process.env.DB_HOST;
-// const { DB_HOST } = require("./config");
+// const DB_HOST = process.env.DB_HOST;
 console.log(DB_HOST);
 
 mongoose.set("strictQuery", true);
