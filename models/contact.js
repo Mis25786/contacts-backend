9152a169fd553824216e90069dd4ce00a36439e2
Mis25786 <111245815+Mis25786@users.noreply.github.com>
2023-06-13 22:36:13 +0200

    error validation phone

--- models/contact.js
+++ models/contact.js
@@ -15,13 +15,13 @@ const contactSchema = new Schema(
     },
     phone: {
       type: String,
-      validate: {
-        validator: function (v) {
-          return /\d{3}-\d{3}-\d{4}/.test(v);
-        },
-        message: (props) => `${props.value} is not a valid phone number!`,
-      },
-      // match: /^\d{3} \d{3}-\d{4}$/,
+      // validate: {
+      //   validator: function (v) {
+      //     return /\d{3}-\d{3}-\d{4}/.test(v);
+      //   },
+      //   message: (props) => `${props.value} is not a valid phone number!`,
+      // },
+      match: /^\d{3}-\d{3}-\d{4}$/,
       required: true,
     },
     favorite: {
