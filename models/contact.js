24aed6075c3bc3dd3e49f969921a4e615da55004
Mis25786 <111245815+Mis25786@users.noreply.github.com>
2023-06-13 22:26:32 +0200

    error valid phone number

--- models/contact.js
+++ models/contact.js
@@ -15,7 +15,13 @@ const contactSchema = new Schema(
     },
     phone: {
       type: String,
-      match: /^\d{10}$/,
+      validate: {
+        validator: function (v) {
+          return /\d{3}-\d{3}-\d{4}/.test(v);
+        },
+        message: (props) => `${props.value} is not a valid phone number!`,
+      },
+      // match: /^\d{3} \d{3}-\d{4}$/,
       required: true,
     },
     favorite: {
