0ffb302b69f6738825029fff34c5d58a778c1aa2
Mis25786 <111245815+Mis25786@users.noreply.github.com>
2023-06-18 20:19:03 +0200

    create user POST work

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,18 +1,13 @@
 const express = require("express");
 
-const controllers = require("../../controllers/auth");
+const ctrl = require("../../controllers/auth");
 const { validateBody } = require("../../middlewares");
 console.log("validateBody-fc :>> ", validateBody);
 const { schemas } = require("../../models/user");
-// console.log("schemas :>> ", schemas);
 
 const router = express.Router();
 
 // router.post("/register", isValidId, controllers.register);
-router.post(
-  "/register",
-  validateBody(schemas.registerSchema),
-  controllers.register
-);
+router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
 
 module.exports = router;
