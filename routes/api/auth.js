2c19122d4e44a262b5dbc22af579cb8d216c2fb6
Mis25786 <111245815+Mis25786@users.noreply.github.com>
2023-06-19 11:48:36 +0200

    create secret_key jwt

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -6,6 +6,10 @@ const { schemas } = require("../../models/user");
 
 const router = express.Router();
 
+// signup
 router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
+
+// signin
+router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
 
 module.exports = router;
