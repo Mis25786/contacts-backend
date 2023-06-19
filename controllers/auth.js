1027d0971a5eceb42f633693da9473d7322f386c
Mis25786 <111245815+Mis25786@users.noreply.github.com>
2023-06-19 12:09:44 +0200

    token error - secretOrPrivateKey must have a value

--- controllers/auth.js
+++ controllers/auth.js
@@ -29,10 +29,13 @@ const register = async (req, res) => {
 const login = async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
+
   if (!user) {
     throw HttpError(401, "Email or password invalid");
   }
+
   const passwordCompare = await bcrypt.compare(password, user.password);
+
   if (!passwordCompare) {
     throw HttpError(401, "Email or password invalid");
   }
